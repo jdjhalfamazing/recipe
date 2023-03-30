f6b63f03e1cb5ff9d3b64bfaf7a00023ffd82e03
Joseph Jordan <jordanjoseph25@yahoo.com>
2023-03-29 23:16:47 -0500

    first change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function RecipeApp() {
+  const [recipes, setRecipes] = useState([
+    { name: "Pasta with Tomato Sauce", ingredients: ["pasta", "tomato sauce"] },
+    { name: "Chicken Curry", ingredients: ["chicken", "curry powder", "coconut milk"] },
+    { name: "Grilled Cheese Sandwich", ingredients: ["bread", "cheese"] }
+  ]);
+
+  const [newRecipeName, setNewRecipeName] = useState("");
+  const [newRecipeIngredients, setNewRecipeIngredients] = useState("");
+
+  const handleNewRecipeNameChange = (event) => {
+    setNewRecipeName(event.target.value);
+  }
+
+  const handleNewRecipeIngredientsChange = (event) => {
+    setNewRecipeIngredients(event.target.value);
+  }
+
+  const handleAddRecipe = () => {
+    const newRecipe = {
+      name: newRecipeName,
+      ingredients: newRecipeIngredients.split(",")
+    };
+
+    setRecipes([...recipes, newRecipe]);
+    setNewRecipeName("");
+    setNewRecipeIngredients("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Recipe App</h1>
+      <ul>
+        {recipes.map((recipe, index) => (
+          <li key={index}>
+            <h2>{recipe.name}</h2>
+            <p>Ingredients: {recipe.ingredients.join(", ")}</p>
+          </li>
+        ))}
+      </ul>
+      <h2>Add a New Recipe</h2>
+      <label htmlFor="new-recipe-name">Name:</label>
+      <input type="text" id="new-recipe-name" value={newRecipeName} onChange={handleNewRecipeNameChange} />
+      <br />
+      <label htmlFor="new-recipe-ingredients">Ingredients:</label>
+      <input type="text" id="new-recipe-ingredients" value={newRecipeIngredients} onChange={handleNewRecipeIngredientsChange} />
+      <br />
+      <button onClick={handleAddRecipe}>Add Recipe</button>
     </div>
   );
 }
 
-export default App;
+export default RecipeApp;
